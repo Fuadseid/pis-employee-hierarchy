feb7499476f154c72a268a5e6bb2f229f624168c
Fuadseid <fuaddbus@gmail.com>
2025-01-21 23:27:15 +0300

    finishing static registration page

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      animation: {
+        'fade-in': 'fadeIn 1s ease-in-out',
+      },
+    },
+  },
+  plugins: [],
+};
