c94eecdfbac41bec3957762fe834663a5bbd8857
naol <naolmeseret22@gmail.com>
2025-01-22 11:01:32 +0300

    adding a routing and folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
-import Form from "./Form";
-
-function App() {
-  return <Form />;
-}
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import Form from "./pages/Form";
+import TreeView from "./pages/TreeView";
+const route = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/form",
+    element: <Form />,
+  },
+  {
+    path: "/treeview",
+    element: <TreeView />,
+  },
+]);
+const App = () => {
+  return <RouterProvider router={route} />;
+};
 
 export default App;
