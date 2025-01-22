0439c3c0f8edd1916ad0c3734e264deb78b01599
naol <naolmeseret22@gmail.com>
2025-01-22 23:21:52 +0300

    adding componets and navigation bar to the app and delete the data from the tree and displaying the data from the mockapi

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,24 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home";
 import Form from "./pages/Form";
 import TreeView from "./pages/TreeView";
+import Applayout from "./pages/Applayout";
 const route = createBrowserRouter([
   {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/form",
-    element: <Form />,
-  },
-  {
-    path: "/treeview",
-    element: <TreeView />,
+    element: <Applayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/form",
+        element: <Form />,
+      },
+      {
+        path: "/treeview",
+        element: <TreeView />,
+      },
+    ],
   },
 ]);
 const App = () => {
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ export default {
   theme: {
     extend: {
       animation: {
-        'fade-in': 'fadeIn 1s ease-in-out',
+        "fade-in": "fadeIn 1s ease-in-out",
       },
     },
   },
